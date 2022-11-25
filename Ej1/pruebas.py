5395b7dda763ba1fa563232741e6c0b534d6a11c
Jorge <jorgediamanto01@gmail.com>
2022-11-25 21:39:31 +0100

    cambios

--- Ej1/pruebas.py
+++ Ej1/pruebas.py
@@ -29,33 +29,52 @@ class Huffman:
             self.root = Node(x)
 
         else:
-            if len(x[1])%2==0 and len(x[1])!=1:
+            if len(x[0])%2==0 and len(x[0])!=1:
 
 
-                if current.left==None:
-                    xx=x
-                    x1 = [[],0]
-                    y1=len(x[1])/2
-                    a=0
-                    for y in x[1]:
-                        x1[0]+=y
-                        a+=1
-                        if a==y1:
-                            break
+                
+                xx=x
+                x1 = [[],0]
+                x2 = [[],0]
+                y1=len(x[0])/2
+                a=0
+                for y in x[0]:
+                    x1[0]+=y
+                    a+=1
+                    if a==y1:
+                        break
 
-                    for y in x1[0]:
-                        for yy in x3:
-                            if y == yy[1]:
-                                x1[1]=round(x1[1]+yy[0],2)
+                for y in x1[0]:
+                    for yy in x3:
+                        if y == yy[1]:
+                            x1[1]=round(x1[1]+yy[0],2)
+                
+                print(x1)
+
+                xx[0].reverse()
+
+                a=0
+
+                for y in xx[0]:
+                    x2[0]+=y
+                    a+=1
+                    if a==y1:
+                        break
+                x2[0].reverse()
+
+                for y in x2[0]:
+                    for yy in x3:
+                        if y == yy[1]:
+                            x2[1]=round(x2[1]+yy[0],2) 
+
+                print(x2)
+
+
+                current.left = Node(x1)
+
+                
                     
-                    print(x1)
-
-
-                    current.left = Node()
-
-                if current.right==None:
-                    xx=x
-                    current.right = Node()
+                current.right = Node(x2)
             else:
                 pass
 
@@ -64,6 +83,7 @@ class Huffman:
 x3 = [[0.2,"A"],[0.17,"F"],[0.13,"1"],[0.21,"3"],[0.09,"M"],[0.15,"T"]]
 
 x = estandarizar(x3)
+x.reverse()
 print(x)
 
 a = Huffman()
