e340f05fb440c070f79c3d9346e855161b536905
Jorge <jorgediamanto01@gmail.com>
2022-11-25 20:54:13 +0100

    cambiois

--- Ej1/pruebas2.py
+++ Ej1/pruebas2.py
@@ -1,7 +1,27 @@
-x=[1.0, ['3', 'A', 'F', 'T', '1', 'M', '0']]
+x=[1.0, ['3', 'A', 'F', 'T', '1', 'M']]
+xx=x
+x1 = []
+x2 = []
+y1=len(x[1])/2
+a=0
+for y in x[1]:
+    x1+=y
+    a+=1
+    if a==y1:
+        break
 
+xx[1].reverse()
 
+a=0
 
-print(len(x[1]))
+for y in xx[1]:
+    x2+=y
+    a+=1
+    if a==y1:
+        break
+x2.reverse()
+
+print(x1)
+print(x2)
 
 
