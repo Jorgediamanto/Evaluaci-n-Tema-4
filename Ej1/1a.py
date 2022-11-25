6e1c3869a771f9828fbfc7a78f7e095ac8cfa8c3
Jorge <jorgediamanto01@gmail.com>
2022-11-25 20:09:59 +0100

    cambios

--- Ej1/1a.py
+++ Ej1/1a.py
@@ -1,6 +1,7 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self,value,frequency):
         self.value = value
+        self.frequency = frequency
         self.right = None
         self.left = None
 
@@ -14,4 +15,13 @@ class Huffman:
 
         else:
             if value < current.value:
-                if current.left == None
+                if current.left == None:
+                    current.left= Node(value)
+                else:
+                    self.add(current.left,value)
+
+            else:
+                if current.right == None:
+                    current.right= Node(value)
+                else:
+                    self.add(current.right,value)
